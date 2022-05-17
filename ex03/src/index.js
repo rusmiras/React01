6a432fba67b887982e1c4b868076531c2120e6d5
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-05-17 15:08:24 +0200

    Created second react app

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//add the following code below this line
+
+class App extends React.Component{
+    render() {
+        return (
+            <div className='App'>
+                <h1>Hello, Padawans!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
